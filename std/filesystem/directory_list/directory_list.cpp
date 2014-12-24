f3c4a5e5dd85fd3a72b4ba4c2119c2d416187038
sim9108 <cafri896>
2014-12-24 09:15:21 +0900

    폴더 목록 예제 추가

---
+++ std/filesystem/directory_list/directory_list.cpp
@@ -0,0 +1,36 @@
+// directory_list.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <vector>
+
+
+void folderlist(std::tr2::sys::path root, std::vector<std::tr2::sys::path>& filelist){
+	using namespace std::tr2::sys;
+
+	directory_iterator path_end;
+	directory_iterator itr;
+	for (itr = root; itr != path_end; ++itr){
+		if (is_directory(itr->path())){
+			folderlist(*itr, filelist);
+			continue;
+		}
+		filelist.push_back(itr->path());
+	}
+}
+
+int _tmain(int argc, _TCHAR* argv[]){
+	std::string mdir = ".";
+	std::vector<std::tr2::sys::path> filelist;
+	folderlist(std::tr2::sys::path(mdir), filelist);
+
+	for (auto& item : filelist){
+		std::string mpath = item.string().substr(mdir.size() + 1);
+		std::cout << mpath << std::endl;
+	}
+
+	return 0;
+}
+
